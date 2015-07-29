47a6a7a0082623e8b46409462d1d722368bdb499
Martin Jensen <martin2610@gmail.com>
2015-07-29 15:37:02 +0200

    Parsin url from authresponse.

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ var express    = require('express');        // call express
 var exphbs     = require('express-handlebars');
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var url        = require('url');
 
 // Hardcoded stuff
 var clientId = 'A22d2fg224h98k8D7HH21';
-var authServerPath = 'http://pi-auth-server.herokuapp.com/login';
+var authServer = 'http://pi-auth-server.herokuapp.com';
+var clientServer = 'http://pi-client-server.herokuapp.com';
+var redirectUrl = clientServer + '/authresponse';
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -24,9 +27,6 @@ app.set('view engine', 'handlebars');
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 // router.get('/', function(req, res) {
@@ -39,20 +39,27 @@ app.get('/', function (req, res) {
 });
 
 // Redirection to Authentication server's login
-router.get('/login', function(req, res) {	
-	var url = authServerPath + '?clientId=' + clientId;	
+app.get('/login', function(req, res) {	
+	var url = authServer + '/login?clientId=' + clientId + '&redirectUrl=' + encodeURIComponent(redirectUrl);	
 	res.writeHead(302,
 	  {Location: url}
 	);
 	res.end();
 });
+// Redirect from Authorization server with Auth Code
+app.get('/authresponse', function(req, res) {
+	// Request token from Authorization server
+	var authCode = url.parse(req.url,true).query.authCode;
+
+	// Present
+	res.render('authresponse',function(authCode) {
+		authCode: authCode
+	});
+});
 
 
 // more routes for our API will happen here
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
 
 // START THE SERVER
 // =============================================================================
