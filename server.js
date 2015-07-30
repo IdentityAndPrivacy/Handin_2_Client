379a407ef9a7c6f1fd9f3c088ca1095048d300e2
Martin Jensen <martin2610@gmail.com>
2015-07-30 13:23:38 +0200

    Showing information from resource server and me.

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ var querystring =require('querystring');
 // Hardcoded stuff
 var clientId = 'A22d2fg224h98k8D7HH21';
 var authServer = 'https://pi-auth-server.herokuapp.com';
-var clientServer = 'https://pi-client-server.herokuapp.com'; // debug: 'http://localhost:8080';
+var clientServer = 'http://localhost:8080'; // 'https://pi-client-server.herokuapp.com'; // debug: 
+var resourceServer = 'https://pi-resource-server.herokuapp.com';
 var redirectUrl = clientServer + '/authresponse';
 
 // configure app to use bodyParser()
@@ -37,15 +38,15 @@ var token = "";
 
 // Home path
 app.get('/', function (req, res) {
-    res.render('home');
+	res.render('home');
 });
 
 // Redirection to Authentication server's login
 app.get('/login', function(req, res) {	
 	var url = authServer + '/login?clientId=' + clientId + '&redirectUrl=' + encodeURIComponent(redirectUrl);	
 	res.writeHead(302,
-	  {Location: url}
-	);
+		{Location: url}
+		);
 	res.end();
 });
 // Redirect from Authorization server with Auth Code
@@ -56,31 +57,93 @@ app.get('/authresponse', function(req, res) {
 	console.log(reqUrl);
 	
 	https.get(reqUrl, function(response) {
-	  console.log("Got response: " + response.statusCode);
-	  if(response.statusCode === 200){
-	  	response.on('data',function(chunk) {
-	  		console.log('response' + chunk);
-		  	token = JSON.parse(chunk).token;
-		  	console.log(token);
-		  	res.render('authresponse', {
-		  		token: token
-		  	});		
+		console.log("Got response: " + response.statusCode);
+		if(response.statusCode === 200){
+			response.on('data',function(chunk) {
+				console.log('response' + chunk);
+				token = JSON.parse(chunk).token;
+				console.log(token);
 
-		});
-	  }
-	  else
-	  {
-	  	res.render('error', {
-	  		error: response.statusCode
-	  	});
-	  }
-	  
+			  	// Information about "me"
+			  	https.get('https://pi-auth-server.herokuapp.com/me?token='+token, function(meResponse) {
+			  		if (meResponse.statusCode === 200) {
+			  			meResponse.on('data',function(chunk) {
+			  				console.log(chunk);
+			  				var user = JSON.parse(chunk).user;
+			  				console.log('User' + user);
+
+
+			  				getFriends(token, user, res);
+			  				
+			  			});
+			  		}
+			  	});
+
+		  });
+		}
+		else
+		{
+			res.render('error', {
+				error:'(authserver/request-token) ' + response.statusCode
+			});
+		}
+
 	}).on('error', function(e) {	  
-	  res.render('error', {
-	  		error: e.message
-	  	});
+		res.render('error', {
+			error: e.message
+		});
 	});
 });
+
+function getFriends(token, user, res) {
+	var post_data = querystring.stringify({
+      'accessToken': token
+  	});
+  	console.log('Data: ' + post_data);
+
+  	var post_options = {
+  		host: 'pi-resource-server.herokuapp.com',
+  		path: '/api/token',
+  		method: 'POST',
+  		headers: {
+  			'Content-Type': 'application/x-www-form-urlencoded',
+  			'Content-Length': post_data.length
+  		}
+  	};
+
+  	var post_req = https.request(post_options, function(resourceRes) {
+  		console.log('ANSWER!');  		
+  		var data = '';
+  		resourceRes.on('data', function (chunk) {
+  			console.log('bigger chunk');
+	        data += chunk;
+      	});
+      	resourceRes.on('end', function(){
+	        console.log(data);
+	        if(data){
+  				// Render page
+  				console.log('Data exists!');
+		      	var jsonObject = JSON.parse(data);
+		      	//console.log('jsonObject' + jsonObject);
+		      	// Show view
+			    res.render('authresponse', {
+			  		token: token,
+			  		name: user.firstname + ' ' + user.lastname,
+			  		users: jsonObject.data.users
+			  	});
+  			}
+  			else{
+  				res.render('error', {
+					error: '(resource) ' + resourceRes.statusCode
+				});
+  			}
+	    })
+  	});
+
+    post_req.write(post_data);
+    post_req.end();
+
+};
 
 // START THE SERVER
 // =============================================================================
